66d9368411747e0308290fcbe0ae6f2c43033d43
NassimBoufalous <83754245+NassimBoufalous@users.noreply.github.com>
2022-09-27 09:52:33 +0200

    Add files via upload
    first commit

---
+++ CORESHOP/index.js
@@ -0,0 +1,103 @@
+
+function setCss(l,z,a)
+{
+    l.style[z] = a;
+   
+}
+
+
+function setClass(l,a)
+{
+    l.classList=a;
+} 
+
+
+
+let img = document.createElement("img");
+img.src="./logo.png";
+setCss(img,"text-align","center");
+
+let p = document.createElement("p");
+let n = document.createElement("navbar");
+n.classList="navbar navbar-expand-lg navbar-light bg-ligh";
+n.style.backgroundColor='#758F69';
+
+
+
+
+let form = document.createElement("form");
+setCss(form,"text-align","right");
+setCss(form,"margin-left","85%");
+
+setClass(form,"form-inline my-2 my-lg-0");
+
+let input = document.createElement("input");
+setClass(input,"form-control mr-sm-2");
+input.type="search";
+input.placeholder="Rechercher";
+setCss(input,"align","right");
+form.appendChild(input);
+
+
+
+let button = document.createElement("button");
+setClass(button,"btn btn-outline-success my-2 my-sm-0");
+button.type="submit";
+
+
+let b= document.createTextNode("Rechercher");
+button.appendChild(b);
+
+setCss(button,"backgroundColor","white");
+setCss(button,"float","right");
+
+form.appendChild(button);
+
+
+
+let a=["Collection","Limités","Support","À propos",];
+
+for (var i = 0; i < a.length; i++) {
+    
+
+
+
+let l = document.createElement("li");
+setClass(l,"nav-link");
+setCss(l,"color","White");
+setCss(l,"font-family","Regular");
+setCss(l,"font-size","42px");
+setCss(l,"margin-left","7%");
+setCss(l,"margin-right","7%");
+
+
+
+let textL=document.createTextNode(a[i]);
+
+
+
+
+l.appendChild(textL);
+
+n.appendChild(l);
+
+
+p.appendChild(n);
+if(i==1)
+n.appendChild(img);
+if(i==a.length-1)
+{
+setCss(l,"margin-right","1%");
+console.log("d");
+}
+
+
+document.body.appendChild(p);
+
+}
+document.body.appendChild(form);
+
+
+
+
+
